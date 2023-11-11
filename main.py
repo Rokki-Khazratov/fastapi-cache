722142094a3b219f6516e6b900d109591d1e0af2
Shamshodbek Khazratov <marco1131mc@gmail.com>
2023-11-11 23:37:12 +0500

    redis connection

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import databases
 import sqlalchemy
 import redis
 
-# Настройки базы данных
+# DB settings
 DATABASE_URL = "postgresql://appple:poklmnji@localhost/fastapicache"
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
@@ -14,3 +16,12 @@ notes = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("content", sqlalchemy.String),
 )
+
+# connection Redis
+redis_cache = redis.StrictRedis(host="localhost", port=6379, db=0)
+
+app = FastAPI()
+
+
+class NoteCreate(BaseModel):
+    content: str
